4887d28c0456d9c484efe9386c14af803995e29a
Daniel Woznicki <tokay89@sbcglobal.net>
2016-02-27 16:20:36 -0800

    Start new bot functionality

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -1,7 +1,8 @@
+require 'pp'
 class BotsController < ApplicationController
 	def new
-		p "*" * 50
-		p params
+		oauth_response = HTTParty.post("https://slack.com/api/oauth.access?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&code=#{params['code']}")
+		redirect_to root_path
 	end
 
 	def create
