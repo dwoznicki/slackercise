85b27a102d626282f225e086d52d45b2dda85f76
Daniel Woznicki <tokay89@sbcglobal.net>
2016-02-28 12:44:20 -0800

    Add small changes to check user#create

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -2,6 +2,7 @@ require 'pp'
 class BotsController < ApplicationController
 	def new
 		oauth_response = HTTParty.post("https://slack.com/api/oauth.access?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&code=#{params['code']}")
+		pp oauth_response
 		redirect_to root_path
 	end
 
